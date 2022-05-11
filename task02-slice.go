bade77ecc2c1d02bedd0381ae2a876114c1a34c5
KarbozovDamir <damirkap89@mail.ru>
2022-05-12 04:33:43 +0600

    solution for task 2

--- task02-slice.go
+++ task02-slice.go
@@ -2,5 +2,28 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	return
+	result = make([]int64, 0, len(input))
+	for i := len(input) - 1; i >= 0; i-- {
+		result = append(result, input[i])
+
+	}
+	return result
+
 }
+
+/*
+// Second solution
+// for i := len(input) - 1; i >= len(input)/2; i-- {
+// 	tmp := len(input) - 1 - i
+// 	input[i], input[tmp] = input[tmp], input[i]
+// }
+// return
+*/
+
+/*
+// For check
+// func main() {
+// 	a := []int64{1, 2, 5, 15}
+// 	fmt.Println(a, reverse(a))
+// }
+*/
